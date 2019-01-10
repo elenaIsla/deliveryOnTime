3cfaa2808e312b44fd88030f818c9f851d2ea409
elenaIsla <elisfa28@gmail.com>
2019-01-10 19:46:09 +0100

    the player is moving around the playground with the keys

--- js/game.js
+++ js/game.js
@@ -15,13 +15,41 @@ let playground = [[1,1,1,1,1,1],
                         [1,0,2,2,0,1],
                         [1,0,0,0,0,1],
                         [1,1,1,1,1,1]];
+let player = {
+    x:2,
+    y:3
+}
 
 function start(){
     displayGame();
     buildPlayground();
+    assignControlsToKeys();   
 }
-
-function displayGame(){
+function displayStartPage() {
+    const startPage = document.getElementById('start-page');
+    startPage.style.display = 'block';
+    const titleGame = document.createElement('h1');
+    titleGame.innerHTML = "Delibery on Time";
+    const divTitleGame = document.createElement('div');
+    divTitleGame.setAttribute("class", "class-title-start-page");
+    divTitleGame.appendChild(titleGame);
+    startPage.appendChild(divTitleGame);
+    const divImgesBoxes = document.createElement('div');
+    divImgesBoxes.setAttribute("class", "class-img-start-page");
+    const images = document.createElement('img'); 
+    divImgesBoxes.appendChild(images);
+    divImgesBoxes.appendChild(images);
+    divImgesBoxes.appendChild(images);
+    startPage.appendChild(divImgesBoxes);
+    const divButtonStart = document.createElement('div');
+    divButtonStart.setAttribute("class", "class-button-start");
+    const button = document.createElement('button');
+    button.setAttribute("id", "button-start");
+    button.innerHTML = "START";
+    divButtonStart.appendChild(button);
+    startPage.appendChild(divButtonStart);
+}
+function displayGame() {
     const buttonStart = document.getElementById('button-start');
     buttonStart.onclick = function(){
         // const nonplayStart = document.getElementById('start-page').style.display = 'none';
@@ -37,16 +65,19 @@ function displayGame(){
     }
 }
 
-function buildPlayground (){
-    
+function buildPlayground (){   
+
     let playgroundHtml = document.getElementById('playground');
     for(let i = 0; i < playground.length; i++){
         for(let j = 0; j < playground[i].length; j++){
             let boxHtml = document.createElement('div');
-            boxHtml.setAttribute("class", "box-playground");
+            boxHtml.setAttribute("id",`${i}${j}`);           
             switch(playground[i][j]){
+
 //0 es el terreno; 1 es una barrera; 2 es la caja; 3 el jugador
+
                 case 0:
+                    boxHtml.setAttribute("class", "box-playground");
                     playgroundHtml.appendChild(boxHtml);
                     break;                       
                 case 1:
@@ -66,10 +97,72 @@ function buildPlayground (){
     }
 }
 
+function assignControlsToKeys (){
+    document.onkeydown = (e) => {
+      switch (e.keyCode) {
+        case 38: //arrow up
+          goUp();
+          break;
+        case 40: //arror down
+          goDown();
+          break;
+        case 37: //arror left
+          goLeft();
+          break;
+        case 39: //arrow right
+          goRight();
+          break; 
+      }
+    };
+  }
+function goUp(){
+    var playerBox = document.getElementsByClassName('box-player')[0];
+    if(playground[player.x - 1][player.y] === 0 ){
+        playerBox.className = "box-playground";
+        document.getElementById(`${player.x -1}${player.y}`).className = "box-player";       
+        playground[player.x][player.y] = 0;
+        playground[player.x - 1][player.y] = 3;
+        player.x = player.x - 1;
+    }
+}
+
+function goRight(){
+    var playerBox = document.getElementsByClassName('box-player')[0];
+    if(playground[player.x][player.y + 1] === 0 ){
+        playerBox.className = "box-playground";
+        document.getElementById(`${player.x}${player.y + 1}`).className = "box-player"; 
+        playground[player.x][player.y] = 0;
+        playground[player.x][player.y + 1] = 3;
+        player.y = player.y + 1;
+    }
+}
+
+function goDown(){
+    var playerBox = document.getElementsByClassName('box-player')[0];
+    if(playground[player.x + 1][player.y] === 0 ){
+        playerBox.className = "box-playground";
+        document.getElementById(`${player.x + 1}${player.y}`).className = "box-player"; 
+        playground[player.x][player.y] = 0;
+        playground[player.x + 1][player.y] = 3;
+        player.x = player.x + 1;
+    }
+}
+
+function goLeft(){
+    var playerBox = document.getElementsByClassName('box-player')[0];
+    if(playground[player.x][player.y - 1] === 0 ){
+        playerBox.className = "box-playground";
+        document.getElementById(`${player.x}${player.y - 1}`).className = "box-player"; 
+        playground[player.x][player.y] = 0;
+        playground[player.x][player.y - 1] = 3;
+        player.y = player.y - 1;
+    }
+}
 
 
 
 
 document.onload = function (){
+    displayStartPage();
     start();
 }();
