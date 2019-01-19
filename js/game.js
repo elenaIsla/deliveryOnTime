6dffe7088bfe4ef381ee56918ee47973129c5456
elenaIsla <elisfa28@gmail.com>
2019-01-19 13:01:33 +0100

    introduce new levels

--- js/game.js
+++ js/game.js
@@ -1,64 +1,36 @@
-//de la página inicial al inicio del juego
-// class Game{
-//     constructor(){
-//     this.playground = [[0,0,0,0,],
-//                         [0,1,p,0],
-//                         [0,b,b,0],
-//                         [0,0,0,0],
-//                         [0,0,0,0]];
-//     }
-// }
-let playground = [[1,1,1,1,1,1],
-                        [1,0,0,0,0,1],
-                        [1,0,1,3,0,1],
-                        [1,0,2,2,0,1],
-                        [1,0,0,2,0,1],
-                        [1,0,0,0,0,1],
-                        [1,1,1,1,1,1]];
-
-let playgroundSolution = [["","","","","",""],
-                        ["","","","","",""],
-                        ["","","","","",""],
-                        ["","","",10,"",""],
-                        ["","",10,10,"",""],
-                        ["","","","","",""],
-                        ["","","","","",""]];                       
-let player = {
-    x:2,
-    y:3
-}
-let numberBoxWin = 3;
+let level = 2;
+let game;
 let counterMovimentsPlayer = 0;
 let counterPushesBox = 0;
 
-function start(){
-    
+function start(){  
+    game = new Playground (level); 
     displayGame();
     reStart();
-    buildPlayground();
-    assignControlsToKeys();
-    
-     
+    //passNextLevel();
+    game.asignPlayground();
+    game.buildPlayground(); 
+    assignControlsToKeys();    
 }
 
 function reStart (){
     const buttonReStart = document.getElementById('button-reset');
+    const buttonTryAgain = document.getElementById('button-try-again');
     buttonReStart.onclick = function (){
-        playground = [[1,1,1,1,1,1],
-                        [1,0,0,0,0,1],
-                        [1,0,1,3,0,1],
-                        [1,0,2,2,0,1],
-                        [1,0,0,2,0,1],
-                        [1,0,0,0,0,1],
-                        [1,1,1,1,1,1]];
-        player.x = 2;
-        player.y = 3;
-        counterMovimentsPlayer = 0;
         resetCounter();
         deleteBackground();
-        buildPlayground();
+        game.asignPlayground();
+        game.buildPlayground();
         deleteWinPage();
         console.log("si");
+    }
+}
+
+function passNextLevel (){
+    const buttonNextLevel = document.getElementById('button-next-level');
+    buttonNextLevel.onclick = function (){
+        start();
+        
     }
 }
 
@@ -102,29 +74,15 @@ function displayGame() {
         }
     }
 }
-
-function checkIfBoxisRightPosition(BoxPositionX,BoxPositionY){
-    if(playgroundSolution[BoxPositionX][BoxPositionY] === 10){
-        console.log("hola");
-        return true;        
-    }
-}
-
-function checkIfisBoxSolution(BoxPositionX,BoxPositionY){
-    if(playgroundSolution[BoxPositionX][BoxPositionY] === 10 && playground[BoxPositionX][BoxPositionY] === 0){
-        return true;
-    }
-}
-
 function checkWinOrNot (){
     
     let boxDelivery = 0;
-    playground.forEach(function(row, i){
+    game.playground.forEach(function(row, i){
         row.forEach(function(colum, j){
-            if(colum == 2 && playgroundSolution[i][j] == 10){
+            if(colum == 2 && game.playgroundSolution[i][j] == 10){
                 boxDelivery = boxDelivery + 1;
                 console.log(boxDelivery)
-                if(boxDelivery == numberBoxWin){
+                if(boxDelivery == game.numberBoxWin){
                     displayWinPage();
                     console.log("win");
                 }
@@ -134,68 +92,40 @@ function checkWinOrNot (){
 }
 
 function displayWinPage (){
+
+    //----borramos el reset para ponerel "try again"-----
+    let divResetButton = document.getElementById('div-button-reset');
+    divResetButton.removeChild(document.getElementById('button-reset'));
+
     let divGame = document.getElementById('game-page');
     let divWin = document.createElement('div');
     divWin.setAttribute('class','class-win-page');
     divWin.innerHTML = "YOU WIN!!!";
     divGame.appendChild(divWin);
+    let divButtons = document.createElement('div');
+    divButtons.setAttribute('class','class-win-page-buttons');
+    divGame.appendChild(divButtons);
+    let newLevelButton = document.createElement ('button');
+    newLevelButton.innerHTML = "Next Level";
+    newLevelButton.setAttribute('id','button-next-level');
+    let resetButton = document.createElement ('button');
+    resetButton.setAttribute('id','button-try-again');
+    resetButton.innerHTML = "Try again";
+    divButtons.appendChild(newLevelButton);
+    divButtons.appendChild(resetButton);
+    level ++;
+    passNextLevel();
 }
 
 function deleteWinPage (){
     let divGamePage = document.getElementById('game-page');
     divGamePage.removeChild(document.getElementsByClassName('class-win-page')[0]);
+    divGamePage.removeChild(document.getElementsByClassName('class-win-page-buttons')[0]);
 }
 
 function deleteBackground (){
     const divPlayground = document.getElementById('div-playground');
     divPlayground.removeChild(document.getElementById('playground'));
-}
-
-function buildPlayground (){   
-    
-    let divPlayground = document.getElementById('div-playground');
-    let playgroundHtml = document.createElement('div');
-    playgroundHtml.setAttribute("id","playground");
-    playgroundHtml.setAttribute("class","class-playground");
-    divPlayground.appendChild(playgroundHtml);
-    // let playgroundHtml = document.getElementById('playground');
-
-    for(let i = 0; i < playground.length; i++){
-        for(let j = 0; j < playground[i].length; j++){
-            let boxHtml = document.createElement('div');
-            boxHtml.setAttribute("id",`${i}${j}`);           
-            switch(playground[i][j]){
-
-//0 es el terreno; 1 es una barrera; 2 es la caja; 3 el jugador
-
-                case 0:
-                    if(checkIfisBoxSolution(i,j)){
-                        boxHtml.setAttribute("class", "box-playground-solution");
-                    }else{
-                        boxHtml.setAttribute("class", "box-playground");
-                    }
-                    playgroundHtml.appendChild(boxHtml);
-                    break;                       
-                case 1:
-                    boxHtml.setAttribute("class", "box-wall");
-                    playgroundHtml.appendChild(boxHtml);                   
-                    break;
-                case 2:
-                    if(checkIfBoxisRightPosition(i,j)){
-                        boxHtml.setAttribute("class","box-deliver-on-time");                                       
-                        playgroundHtml.appendChild(boxHtml);
-                    }else{
-                        boxHtml.setAttribute("class","box-deliver");                                       
-                        playgroundHtml.appendChild(boxHtml); 
-                    }                  
-                    break; 
-                case 3:
-                    boxHtml.setAttribute("class","box-player");                  
-                    playgroundHtml.appendChild(boxHtml);                    
-                    break;                      
-            }
-        }     
-    }
 }
 
 function assignControlsToKeys (){
@@ -218,8 +148,8 @@ function assignControlsToKeys (){
 }
   
 function movePlayer(PlayerPositionX,PlayerPositionY,direction){
-    playground[PlayerPositionX][PlayerPositionY] = 0;
-    if(checkIfisBoxSolution(PlayerPositionX, PlayerPositionY)){
+    game.playground[PlayerPositionX][PlayerPositionY] = 0;
+    if(game.checkIfisBoxSolution(PlayerPositionX, PlayerPositionY)){
         document.getElementsByClassName('box-player')[0].className = "box-playground-solution"; 
     }else{
         document.getElementsByClassName('box-player')[0].className = "box-playground";
@@ -227,26 +157,26 @@ function movePlayer(PlayerPositionX,PlayerPositionY,direction){
     switch (direction){
         case "up":
             document.getElementById(`${PlayerPositionX - 1}${PlayerPositionY}`).className = "box-player";       
-            playground[PlayerPositionX - 1][PlayerPositionY] = 3;
-            player.x = PlayerPositionX - 1;
+            game.playground[PlayerPositionX - 1][PlayerPositionY] = 3;
+            game.player.x = PlayerPositionX - 1;
             counterMoviments();
             break;
         case "down":
             document.getElementById(`${PlayerPositionX + 1}${PlayerPositionY}`).className = "box-player";                   
-            playground[PlayerPositionX + 1][PlayerPositionY] = 3;
-            player.x = PlayerPositionX + 1;
+            game.playground[PlayerPositionX + 1][PlayerPositionY] = 3;
+            game.player.x = PlayerPositionX + 1;
             counterMoviments();
             break;
         case "left":
             document.getElementById(`${PlayerPositionX}${PlayerPositionY - 1}`).className = "box-player";                  
-            playground[PlayerPositionX][PlayerPositionY - 1] = 3;
-            player.y = PlayerPositionY - 1;
+            game.playground[PlayerPositionX][PlayerPositionY - 1] = 3;
+            game.player.y = PlayerPositionY - 1;
             counterMoviments();
             break;
         case "right":
             document.getElementById(`${PlayerPositionX}${PlayerPositionY + 1}`).className = "box-player";                  
-            playground[PlayerPositionX][PlayerPositionY + 1] = 3;
-            player.y = PlayerPositionY + 1;
+            game.playground[PlayerPositionX][PlayerPositionY + 1] = 3;
+            game.player.y = PlayerPositionY + 1;
             counterMoviments();
             break;
     }
@@ -255,46 +185,46 @@ function movePlayer(PlayerPositionX,PlayerPositionY,direction){
 function moveBox(BoxPositionX,BoxPositionY,direction){
     switch(direction){
         case "up":
-            if(checkIfBoxisRightPosition(BoxPositionX - 2,BoxPositionY)){
+            if(game.checkIfBoxisRightPosition(BoxPositionX - 2,BoxPositionY)){
                 document.getElementById(`${BoxPositionX - 2}${BoxPositionY}`).className = "box-deliver-on-time";
             }else{
                 document.getElementById(`${BoxPositionX - 2}${BoxPositionY}`).className = "box-deliver";
             }
-            playground[BoxPositionX - 1][BoxPositionY] = 0;
-            playground[BoxPositionX - 2][BoxPositionY] = 2;
+            game.playground[BoxPositionX - 1][BoxPositionY] = 0;
+            game.playground[BoxPositionX - 2][BoxPositionY] = 2;
             counterPushes();
             checkWinOrNot();
             break;
         case "down":
-            if(checkIfBoxisRightPosition(BoxPositionX + 2,BoxPositionY)){
+            if(game.checkIfBoxisRightPosition(BoxPositionX + 2,BoxPositionY)){
                 document.getElementById(`${BoxPositionX + 2}${BoxPositionY}`).className = "box-deliver-on-time";
             }else{
                 document.getElementById(`${BoxPositionX + 2}${BoxPositionY}`).className = "box-deliver";
             }
-            playground[BoxPositionX + 1][BoxPositionY] = 0;
-            playground[BoxPositionX + 2][BoxPositionY] = 2;
+            game.playground[BoxPositionX + 1][BoxPositionY] = 0;
+            game.playground[BoxPositionX + 2][BoxPositionY] = 2;
             counterPushes();
             checkWinOrNot();
             break;
         case "left":
-            if(checkIfBoxisRightPosition(BoxPositionX,BoxPositionY - 2)){
+            if(game.checkIfBoxisRightPosition(BoxPositionX,BoxPositionY - 2)){
                 document.getElementById(`${BoxPositionX}${BoxPositionY - 2}`).className = "box-deliver-on-time";
             }else{
                 document.getElementById(`${BoxPositionX}${BoxPositionY - 2}`).className = "box-deliver";
             }
-            playground[BoxPositionX][BoxPositionY - 1] = 0;
-            playground[BoxPositionX][BoxPositionY - 2] = 2;
+            game.playground[BoxPositionX][BoxPositionY - 1] = 0;
+            game.playground[BoxPositionX][BoxPositionY - 2] = 2;
             counterPushes();
             checkWinOrNot();
             break;
         case "right":
-            if(checkIfBoxisRightPosition(BoxPositionX,BoxPositionY + 2)){
+            if(game.checkIfBoxisRightPosition(BoxPositionX,BoxPositionY + 2)){
                 document.getElementById(`${BoxPositionX}${BoxPositionY + 2}`).className = "box-deliver-on-time";
             }else{
                 document.getElementById(`${BoxPositionX}${BoxPositionY + 2}`).className = "box-deliver";
             }
-            playground[BoxPositionX][BoxPositionY + 1] = 0;
-            playground[BoxPositionX][BoxPositionY + 2] = 2;
+            game.playground[BoxPositionX][BoxPositionY + 1] = 0;
+            game.playground[BoxPositionX][BoxPositionY + 2] = 2;
             counterPushes();
             checkWinOrNot();
             break;
@@ -302,38 +232,38 @@ function moveBox(BoxPositionX,BoxPositionY,direction){
 }
 
 function goUp(){
-    if(playground[player.x - 1][player.y] === 0 ){
-        movePlayer(player.x, player.y, "up");
-    }else if(playground[player.x - 1][player.y] === 2 && playground[player.x - 2][player.y] === 0){ 
-        moveBox(player.x, player.y, "up");
-        movePlayer(player.x, player.y, "up");
+    if(game.playground[game.player.x - 1][game.player.y] === 0 ){
+        movePlayer(game.player.x, game.player.y, "up");
+    }else if(game.playground[game.player.x - 1][game.player.y] === 2 && game.playground[game.player.x - 2][game.player.y] === 0){ 
+        moveBox(game.player.x, game.player.y, "up");
+        movePlayer(game.player.x, game.player.y, "up");
     }
 }
 
 function goRight(){
-    if(playground[player.x][player.y + 1] === 0 ){
-        movePlayer(player.x, player.y, "right");
-    }else if(playground[player.x][player.y + 1] === 2 && playground[player.x][player.y + 2] === 0){
-        moveBox(player.x, player.y, "right");
-        movePlayer(player.x, player.y, "right");       
+    if(game.playground[game.player.x][game.player.y + 1] === 0 ){
+        movePlayer(game.player.x,game.player.y, "right");
+    }else if(game.playground[game.player.x][game.player.y + 1] === 2 && game.playground[game.player.x][game.player.y + 2] === 0){
+        moveBox(game.player.x, game.player.y, "right");
+        movePlayer(game.player.x, game.player.y, "right");       
     }
 }
 
 function goDown(){
-    if(playground[player.x + 1][player.y] === 0 ){
-        movePlayer(player.x, player.y, "down");
-    }else if(playground[player.x + 1][player.y] === 2 && playground[player.x + 2][player.y] === 0){
-        moveBox(player.x, player.y, "down");
-        movePlayer(player.x, player.y, "down");        
+    if(game.playground[game.player.x + 1][game.player.y] === 0 ){
+        movePlayer(game.player.x, game.player.y, "down");
+    }else if(game.playground[game.player.x + 1][game.player.y] === 2 && game.playground[game.player.x + 2][game.player.y] === 0){
+        moveBox(game.player.x, game.player.y, "down");
+        movePlayer(game.player.x, game.player.y, "down");        
     }
 }
 
 function goLeft(){
-    if(playground[player.x][player.y - 1] === 0 ){
-        movePlayer(player.x, player.y, "left");
-    }else if(playground[player.x][player.y - 1] === 2 && playground[player.x][player.y - 2] === 0){
-        moveBox(player.x, player.y, "left");
-        movePlayer(player.x, player.y, "left");        
+    if(game.playground[game.player.x][game.player.y - 1] === 0 ){
+        movePlayer(game.player.x, game.player.y, "left");
+    }else if(game.playground[game.player.x][game.player.y - 1] === 2 && game.playground[game.player.x][game.player.y - 2] === 0){
+        moveBox(game.player.x, game.player.y, "left");
+        movePlayer(game.player.x, game.player.y, "left");        
     }
 }
 
@@ -344,6 +274,9 @@ function counterMoviments (){
 
 function resetCounter (){
     document.getElementById('num-moves').innerHTML = 0;
+    document.getElementById('num-push').innerHTML = 0;
+    counterMovimentsPlayer = 0;
+    counterPushesBox = 0;
 
 }
 
@@ -352,15 +285,7 @@ function counterPushes (){
     document.getElementById('num-push').innerHTML = counterPushesBox;
 }
 
-function resetCounter (){
-    document.getElementById('num-push').innerHTML = 0;
-
-}
-
-
-
 document.onload = function (){
     displayStartPage();
     start();
-    
 }();
---
+++ js/playground.js
@@ -0,0 +1,119 @@
+class Playground {
+    constructor (level){
+        this.level = level;
+        this.playground = [];
+        this.playgroundSolution = [];
+        this.player = {x:0, y:0};
+        this.numberBoxWin = 0;
+    }
+
+ asignPlayground (){
+     if(this.level === 1){
+        this.playground = [[1,1,1,1,1,1],
+                            [1,0,0,0,0,1],
+                            [1,0,1,3,0,1],
+                            [1,0,2,2,0,1],
+                            [1,0,0,2,0,1],
+                            [1,0,0,0,0,1],
+                            [1,1,1,1,1,1]];
+        this.playgroundSolution = [["","","","","",""],
+                                    ["","","","","",""],
+                                    ["","","","","",""],
+                                    ["","","",10,"",""],
+                                    ["","",10,10,"",""],
+                                    ["","","","","",""],
+                                    ["","","","","",""]];
+        this.player.x = 2;
+        this.player.y = 3;
+        this.numberBoxWin = 3;
+    console.log ("nivel 1");
+     }else if (this.level === 2){
+        this.playground = [[1,1,1,1,"",""],
+                            [1,0,0,1,"",""],
+                            [1,0,0,1,1,1],
+                            [1,2,3,0,0,1],
+                            [1,0,0,2,0,1],
+                            [1,0,0,1,1,1],
+                            [1,1,1,1,"",""]];
+        this.playgroundSolution = [["","","","","",""],
+                                    ["","",10,"","",""],
+                                    ["","","","","",""],
+                                    ["",10,"","","",""],
+                                    ["","","","","",""],
+                                    ["","","","","",""],
+                                    ["","","","","",""]];
+        this.player.x = 3;
+        this.player.y = 2;
+        this.numberBoxWin = 2;
+     }
+ }
+
+
+
+ buildPlayground (){ 
+        // this.asignPlayground ();  
+// -------seleccionamos el div / creamos div dentro / le asignamos una id y class / lo juntamos al div padre
+        let divPlayground = document.getElementById('div-playground');
+        let playgroundHtml = document.createElement('div');
+        playgroundHtml.setAttribute("id","playground");
+        playgroundHtml.setAttribute("class","class-playground");
+        divPlayground.appendChild(playgroundHtml);
+
+// ------según el playground construimos un tablero diferente------
+    
+        for(let i = 0; i < this.playground.length; i++){
+            for(let j = 0; j < this.playground[i].length; j++){
+                let boxHtml = document.createElement('div');
+                boxHtml.setAttribute("id",`${i}${j}`);           
+                switch(this.playground[i][j]){
+    
+//--------0 es el terreno; 1 es una barrera; 2 es la caja; 3 el jugador
+    
+                    case 0:
+                        if(this.checkIfisBoxSolution(i,j)){
+                            boxHtml.setAttribute("class", "box-playground-solution");
+                        }else{
+                            boxHtml.setAttribute("class", "box-playground");
+                        }
+                        playgroundHtml.appendChild(boxHtml);
+                        break;                       
+                    case 1:
+                        boxHtml.setAttribute("class", "box-wall");
+                        playgroundHtml.appendChild(boxHtml);                   
+                        break;
+                    case 2:
+                        if(this.checkIfBoxisRightPosition(i,j)){
+                            boxHtml.setAttribute("class","box-deliver-on-time");                                       
+                            playgroundHtml.appendChild(boxHtml);
+                        }else{
+                            boxHtml.setAttribute("class","box-deliver");                                       
+                            playgroundHtml.appendChild(boxHtml); 
+                        }                  
+                        break; 
+                    case 3:
+                        boxHtml.setAttribute("class","box-player");                  
+                        playgroundHtml.appendChild(boxHtml);                    
+                        break; 
+                    case "":
+                        boxHtml.style.backgroundColor = "black";                  
+                        playgroundHtml.appendChild(boxHtml);                    
+                        break;                  
+                }
+            }     
+        }
+    }
+
+    checkIfisBoxSolution(i,j){
+        if(this.playgroundSolution[i][j] === 10 && this.playground[i][j] === 0){
+            return true;
+        }
+    }
+
+    checkIfBoxisRightPosition(i,j){
+        if(this.playgroundSolution[i][j] === 10){
+            console.log("hola");
+            return true;        
+        }
+    }
+
+}
